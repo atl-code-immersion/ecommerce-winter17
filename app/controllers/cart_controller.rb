3b9943b711a534102dfebd39ad70ae8f71e59e68
Aaron Groch <ar.groch@gmail.com>
2017-02-14 12:29:43 -0500

    in checkout action, combined the two each loops into one

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -19,25 +19,20 @@ class CartController < ApplicationController
   end
 
   def checkout
-  	@line_items = LineItem.all
+  	line_items = LineItem.all
   	@order = Order.create(user_id: current_user.id, subtotal: 0)
 
-  	LineItem.pluck(:line_item_total).each do |num|
-  		@order.subtotal += num
+  	line_items.each do |line_item|
+  		line_item.product.update(quantity: (line_item.product.quantity - line_item.quantity))
+  		@order.order_items[line_item.product_id] = line_item.quantity 
+  		@order.subtotal += line_item.line_item_total
   	end
 		@order.save
 
   	@order.update(sales_tax: (@order.subtotal * 0.08))
   	@order.update(grand_total: (@order.sales_tax + @order.subtotal))
 
-  	@line_items.each do |line_item|
-  		line_item.product.update(quantity: (line_item.product.quantity - line_item.quantity))
-  		@order.order_items[line_item.product_id] = line_item.quantity 
-  		@order.save
-  	end
-
-  	@line_items.destroy_all
-
+  	line_items.destroy_all
   end
 end
 
